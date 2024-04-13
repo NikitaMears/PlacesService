7aab91d8befde97f8d70cd584ba37bad27493218
Melaku Minas <minasmelaku@gmail.com>
2024-04-13 14:05:13 +0000

    changes on server

--- index.js
+++ index.js
@@ -1,15 +1,26 @@
 const express = require('express');
 const app = express();
 const placesRoutes = require('./routes/placesRoutes');
+const cors = require('cors');
+
 
 // Middleware to parse JSON bodies
 app.use(express.json());
 
 // Use places routes
+const corsOptions = {
+  origin: ['localhost:3000', 'http://map.ethiomaps.com'],
+};
+
+// Use CORS middleware with specified options
+app.use(cors());
+
+
+
 app.use('/', placesRoutes);
 
 // Start the server
-const PORT = process.env.PORT || 3004;
+const PORT = process.env.PORT || 3002;
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 });
